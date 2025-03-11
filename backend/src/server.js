6f3c8ece3f9df7a448d8fa75ad94f5cdf9be184b
Aaro <aaro.h4malainen@gmail.com>
2025-03-11 15:20:13 +0200

    Socket connection test

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,33 @@ import { root, userRoutes } from './routes/routes.js'
 import dbInit from './database.js'
 import dotenv from "dotenv";
 import path from 'path'
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 
 dotenv.config();
 
 const fastify = Fastify({
-  logger: true
+  //logger: true
 })
+const server = fastify.server;
+
+const io = new Server(server, {
+	cors: {
+	  origin: "*", // Change to frontend URL whenever needed
+	  methods: ["GET", "POST"],
+	  allowedHeaders: ["Content-Type"],
+	  credentials: true
+	},
+  });
+
+  io.on("connection", (socket) => {
+	console.log("A user connected:", socket.id);
+  
+	socket.on("disconnect", () => {
+	  console.log("User disconnected:", socket.id);
+	});
+  });
 
 fastify.register(fastifyStatic, {
   root: path.join(process.cwd(), '../public'),
@@ -20,7 +41,7 @@ await fastify.register(dbInit)
 await fastify.register(root)
 await fastify.register(userRoutes)
 
-fastify.listen({ port: process.env.PORT }, function (err, address) {
+fastify.listen({ port: process.env.PORT || 5000}, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
