df3a0a1f1a004c62491de9d0b6a916ba2f67bd90
PatuLang <patrik.laang@gmail.com>
2025-04-01 16:03:01 +0300

    Getting into it with the frontend, first commit

--- frontend/src/pages/frontpage.ts
+++ frontend/src/pages/frontpage.ts
@@ -1,33 +1,78 @@
 import { route } from "../router/router.js";
 
-export function loadFrontPage(): void {
-    const app = document.getElementById('root')!;
+export function loadFrontPage(): void
+{
+	const app = document.getElementById('root')!;
 	fetch('/templates/frontpage.html')
-	.then(response => {
-		if (!response.ok) {
+	.then(response => 
+	{
+		if (!response.ok)
+		{
 			throw new Error(`Fetch failed: ${response.status}`);
 		}
 		return response.text();
 	})
-	.then(html => {
+	.then(html => 
+	{
 		app.innerHTML = html;
-		
-		document.getElementById('login-button')!.addEventListener('click', (event) => {
-			event.preventDefault();
-			route(event, '/login');
-		});
-
-		document.getElementById('register-button')!.addEventListener('click', (event) => {
-			event.preventDefault();
-			route(event, '/register');
-		});
-
-		document.getElementById('game-debug')!.addEventListener('click', (event) => {
-			route(event, '/game');
-		});
+	
+		app.addEventListener('click', (event) => 
+		{
+			const target = event.target as HTMLElement;
+			
+			if (target.matches('#login-button'))
+			{
+				event.preventDefault();
+				route(event, '/login');
+			}
+			if (target.matches('#register-button'))
+			{
+				event.preventDefault();
+				route(event, '/register');
+			}
+			if (target.matches('#game-debug'))
+			{
+				route(event, '/game');
+			}
+		})
 	})
-	.catch(error => {
+	.catch(error => 
+	{
 		console.error('Error loading login page:', error);
 		alert('Something went wrong. Please try again later.'); //make error handling unified
 	});
 }
+
+// import { route } from "../router/router.js";
+
+// export function loadFrontPage(): void {
+//     const app = document.getElementById('root')!;
+// 	fetch('/templates/frontpage.html')
+// 	.then(response => {
+// 		if (!response.ok) {
+// 			throw new Error(`Fetch failed: ${response.status}`);
+// 		}
+// 		return response.text();
+// 	})
+// 	.then(html => {
+// 		app.innerHTML = html;
+		
+// 		document.getElementById('login-button')!.addEventListener('click', (event) => {
+// 			event.preventDefault();
+// 			route(event, '/login');
+// 		});
+
+// 		document.getElementById('register-button')!.addEventListener('click', (event) => {
+// 			event.preventDefault();
+// 			route(event, '/register');
+// 		});
+
+// 		document.getElementById('game-debug')!.addEventListener('click', (event) => {
+// 			route(event, '/game');
+// 		});
+// 	})
+// 	.catch(error => {
+// 		console.error('Error loading login page:', error);
+// 		alert('Something went wrong. Please try again later.'); //make error handling unified
+// 	});
+// }
--- frontend/src/pages/loginForm.ts
+++ frontend/src/pages/loginForm.ts
@@ -1,37 +1,44 @@
 import { LoginRequest, loginUser } from '../services/api.js';
 import { loadGameRoom } from './gameRoom.js'; // Correct path to gameRoom.ts
 
-export function loadLoginForm(): void {
+export function loadLoginForm(): void
+{
 	const app = document.getElementById('root')!;
 	fetch('/templates/login.html')
-		.then(response => {
-			if (!response.ok) {
-				throw new Error(`Fetch failed: ${response.status}`);
+	.then(response => 
+	{
+		if (!response.ok)
+		{
+			throw new Error(`Fetch failed: ${response.status}`);
+		}
+		return response.text();
+	})
+	.then(html => 
+	{
+		app.innerHTML = html;
+		
+		document.getElementById('login-form')!.addEventListener('submit', async (event) => 
+		{
+			event.preventDefault();
+
+			const name = (document.getElementById('username') as HTMLInputElement).value;
+			const password = (document.getElementById('password') as HTMLInputElement).value;
+
+			const user: LoginRequest = {
+				name,
+				password
+			};
+			const success = await loginUser(user);
+
+			if (success)
+			{
+				// loadGameRoom();
 			}
-			return response.text();
-		})
-		.then(html => {
-			app.innerHTML = html;
-			
-			document.getElementById('login-form')!.addEventListener('submit', async (e) => {
-				e.preventDefault();
-
-				const name = (document.getElementById('username') as HTMLInputElement).value;
-				const password = (document.getElementById('password') as HTMLInputElement).value;
-
-				const user: LoginRequest = {
-					name,
-					password
-				};
-				const success = await loginUser(user);
-
-				if (success) {
-//					loadGameRoom();
-				}
-			});
-		})
-		.catch(error => {
-			console.error('Error loading login page:', error);
-			alert('Something went wrong. Please try again later.'); //make error handling unified
 		});
+	})
+	.catch(error => 
+	{
+		console.error('Error loading login page:', error);
+		alert('Something went wrong. Please try again later.'); //make error handling unified
+	});
 }
