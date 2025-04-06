13a23b5c1ae3c012b402489ea28a534973255ffe
Aaro <aaro.h4malainen@gmail.com>
2025-04-06 12:40:34 +0300

    Fixed building the frontend correctly and removed two useless files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,15 +11,14 @@ const App: React.FC = () => {
 
   return (
   <div>
-	{/*<p>amogus</p>*/}
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        {/*<Route path="/home" element={<Home />} />*/}
-        {/*<Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Registration/>} />
         <Route path="/game" element={<GameRoom/>} />
-        <Route path="*" element={<NoPage/>} />*/}
+        <Route path="*" element={<NoPage/>} />
       </Routes>
     </BrowserRouter>
   </div>
@@ -27,41 +26,3 @@ const App: React.FC = () => {
 }
 
 export default App
-
-// { package.json
-// 	"scripts": {
-// 	"build": "npx tsc --project ./tsconfig.json  && npx tailwindcss -i ./src/input.css -o ./dist/output.css && cp ./src/index.html ./dist/index.html",
-// 	"watch": "npx tsc --project ./tsconfig.json --watch & vite & npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch"
-// 	},
-// 	"dependencies": {
-// 	  "@tailwindcss/cli": "^4.0.8",
-// 	  "socket.io-client": "^4.8.1"
-// 	},
-// 	"devDependencies": {
-// 	  "@types/socket.io-client": "^1.4.36",
-// 	  "tailwindcss": "^4.0.8",
-// 	  "typescript": "^5.8.2",
-// 	  "vite": "^5.2.0"
-// 	}
-//   }
-  
-// { tsconfig.json
-// 	"compilerOptions": {
-// 		"target": "ES6",
-// 		"module": "ESNext",
-// 		"moduleResolution": "Bundler",
-// 		"esModuleInterop": true,
-// 		"allowSyntheticDefaultImports": true,
-// 		"outDir": "./dist",
-// 		"rootDir": "./src",
-// 		"baseUrl": "./",
-// 		"paths": {
-// 			// "socket.io-client": ["node_modules/socket.io-client"],
-// 			"undici-types": ["node_modules/undici-types"]
-// 		},
-// 		"lib": ["dom", "es6"],
-// 		"strict": false
-// 	},
-// 	"include": ["src/**/*"],
-// 	"exclude": ["node_modules"]
-// 	} 
--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -3,32 +3,32 @@ import Header from "../components/headers.js";
 
 const Home: React.FC = () => {
 
-	return ( <p> amogus</p>
-		//<>
-		//{/*<Header />*/}
-		//	<div className="flex flex-col items-center justify-center min-h-screen">
-		//	<div className="bg-white p-6 rounded-lg w-96 flex flex-col gap-4 items-center shadow-md">
-		//	  	<Link 
-		//			to="/login"
-		//			className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
-		//	  	>
-		//			Login
-		//	 	</Link>
-		//	  	<Link 
-		//			to="/register"
-		//			className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
-		//	  	>
-		//			Register
-		//	  	</Link>
-		//	  	<Link 
-		//			to="/game"
-		//			className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
-		//	  	>
-		//			Debug Game
-		//	  	</Link>
-		//	</div>
-		//	</div>
-		//</>
+	return (
+		<>
+		<Header />
+			<div className="flex flex-col items-center justify-center min-h-screen">
+			<div className="bg-white p-6 rounded-lg w-96 flex flex-col gap-4 items-center shadow-md">
+			  	<Link 
+					to="/login"
+					className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
+			  	>
+					Login
+			 	</Link>
+			  	<Link 
+					to="/register"
+					className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
+			  	>
+					Register
+			  	</Link>
+			  	<Link 
+					to="/game"
+					className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
+			  	>
+					Debug Game
+			  	</Link>
+			</div>
+			</div>
+		</>
 	);
 }
 
