c2da3487943610d155d06c5caa187785d13b6dbd
Marek B <mburakow@student.hive.fi>
2025-04-11 16:47:42 +0300

    slime effect

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,35 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 import Header from "../components/headers.js";
+import { startSlimeEffect } from "../effects/slimeEffect";
 
 const Home: React.FC = () => {
+	useEffect(() => {
+		startSlimeEffect();
+	}, []);
 
 	return (
-		<>
-		<Header />
+		<div className="relative z-50">
+			<Header />
 			<div className="flex flex-col items-center justify-center min-h-screen">
-			<div className="bg-white p-6 rounded-lg w-96 flex flex-col gap-4 items-center shadow-md">
-			  	<Link 
-					to="/login"
-					className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
-			  	>
-					Login
-			 	</Link>
-			  	<Link 
-					to="/register"
-					className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
-			  	>
-					Register
-			  	</Link>
-			  	<Link 
-					to="/game"
-					className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center"
-			  	>
-					Debug Game
-			  	</Link>
+				<div className="bg-white p-6 rounded-lg w-96 flex flex-col gap-4 items-center shadow-md">
+					<Link to="/login" className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center">
+						Login
+					</Link>
+					<Link to="/register" className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center">
+						Register
+					</Link>
+					<Link to="/game" className="w-64 bg-green-500 text-white py-2 rounded-md hover:bg-green-700 text-center">
+						Debug Game
+					</Link>
+				</div>
 			</div>
-			</div>
-		</>
+		</div>
 	);
-}
+};
 
-export default Home
+export default Home;
