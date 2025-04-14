5338a6e21b36905c783eec13c96699dbfbf2b212
Marek Burakowski <marek.burakowski@protonmail.com>
2025-04-14 12:34:44 +0300

    check rooms on leave

--- server_auth/database.js
+++ server_auth/database.js
@@ -1,58 +1,66 @@
 import fp from 'fastify-plugin'
+import fs from 'fs'
 import Database from 'better-sqlite3'
 import { Logger, LogLevel } from './utils/logger.js';
 
 const log = new Logger(LogLevel.INFO);
-const dbFile = process.env.DB_FILE || "/data/database.db"
+const dbFile = process.env.DB_FILE || "/app/data/database.db"
 
 async function dbInit(fastify, options) {
-  log.info("Creating database");
-  const db = new Database(dbFile); //, { verbose: console.log })
+    try {
+    log.info("Creating database");
 
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY,
-      google_id TEXT UNIQUE DEFAULT NULL,
-      name TEXT UNIQUE NOT NULL,
-      email TEXT UNIQUE NOT NULL,
-      password TEXT,
-      status INTEGER NOT NULL DEFAULT 0 CHECK(status IN (0 ,1)),
-      wins INTEGER NOT NULL DEFAULT 0,
-      losses INTEGER NOT NULL DEFAULT 0,
-      avatar TEXT NOT NULL,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    );
-  `)
+    fs.mkdirSync('/data', { recursive: true })
 
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS refresh_tokens (
-      id INTEGER PRIMARY KEY,
-      user_id INTEGER,
-      refresh_token TEXT,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-    );
-  `)
+    const db = new Database(dbFile); //, { verbose: console.log })
 
-  db.exec(`
-    CREATE TABLE IF NOT EXISTS friends (
-      user_id INTEGER NOT NULL,
-      friend_id INTEGER NOT NULL,
-      status TEXT NOT NULL CHECK(status IN ('pending', 'accepted', 'blocked')),
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-      PRIMARY KEY (user_id, friend_id),
-      FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-      FOREIGN KEY (friend_id) REFERENCES users(id) ON DELETE CASCADE
-    );
-  `)
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        google_id TEXT UNIQUE DEFAULT NULL,
+        name TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT,
+        status INTEGER NOT NULL DEFAULT 0 CHECK(status IN (0 ,1)),
+        wins INTEGER NOT NULL DEFAULT 0,
+        losses INTEGER NOT NULL DEFAULT 0,
+        avatar TEXT NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        );
+    `)
 
-  fastify.decorate("db", db);
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS refresh_tokens (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        refresh_token TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+        );
+    `)
 
-  fastify.addHook("onClose", (fastify, done) => {
-    db.close();
-    done();
-  });
-  log.info("Database created successfully");
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS friends (
+        user_id INTEGER NOT NULL,
+        friend_id INTEGER NOT NULL,
+        status TEXT NOT NULL CHECK(status IN ('pending', 'accepted', 'blocked')),
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (user_id, friend_id),
+        FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+        FOREIGN KEY (friend_id) REFERENCES users(id) ON DELETE CASCADE
+        );
+    `)
+
+    fastify.decorate("db", db);
+
+    fastify.addHook("onClose", (fastify, done) => {
+        db.close();
+        done();
+    });
+    log.info("Database created successfully");
+    } 	 catch (err) {
+        console.error('[DB INIT FAILED]', err)
+        throw err // optional: just log and continue if desired
+    }
 }
-
 export default fp(dbInit);
--- server_main/src/db/database.js
+++ server_main/src/db/database.js
@@ -1,14 +1,17 @@
 import fp from 'fastify-plugin'
+import fs from 'fs'
 import Database from 'better-sqlite3'
 import { Logger, LogLevel } from '../utils/logger.js';
 import { waitForDBFile, waitForTables} from './wait_db.js';
 
 const log = new Logger(LogLevel.INFO);
 const requiredTables = ['users', 'refresh_tokens', 'friends'];
-const dbFile = process.env.DB_FILE || "/data/database.db"
+const dbFile = process.env.DB_FILE || "/app/data/database.db"
 
 async function dbInit(fastify) {
-  
+  try {
+  fs.mkdirSync('/data', { recursive: true })
+
   await waitForDBFile(dbFile);
 
   const db = new Database(dbFile); //, { verbose: console.log });
@@ -23,6 +26,10 @@ async function dbInit(fastify) {
   });
 
   log.info("Connected to database successfully");
+    } catch (err) {
+        console.error('[DB INIT FAILED]', err)
+        throw err // optional: just log and continue if desired
+    }
 }
 
 export default fp(dbInit);
