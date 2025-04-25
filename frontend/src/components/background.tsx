0f8f09f07e6f95301f45b6ccb98b58ce2b719058
Aaro Hamalainen <aaro.h4malainen@gmail.com>
2025-04-25 16:20:58 +0300

    Cool background

--- frontend/src/components/background.tsx
+++ frontend/src/components/background.tsx
@@ -3,14 +3,16 @@ const Background: React.FC = () => {
 	  <div
 		className="fixed inset-0 -z-10"
 		style={{
-		  backgroundImage: "url('https://images.steamusercontent.com/ugc/922543831008219144/3AD9FCC897208D9E0BB8E26247708BEAC66E259A/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false')",
-		  backgroundSize: "cover",
+		  backgroundImage: "url('/amog.gif')",
+		  backgroundSize: "cover",  // Try changing to "contain" if needed
 		  backgroundPosition: "center",
 		  backgroundRepeat: "no-repeat",
 		  backgroundAttachment: "fixed",
+		  filter: "blur(5px)",
 		}}
 	  ></div>
 	);
   };
   
   export default Background;
+  
